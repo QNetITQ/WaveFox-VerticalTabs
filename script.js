e759b48b752ea324a828591692dbbf7f38d90f85
QNetITQ <85301851+QNetITQ@users.noreply.github.com>
2023-12-02 19:55:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var HTMLRegularTabContainer = document.getElementById("RegularTabContainer");
 
 async function TabList()
 {
-    HTMLPinnedTabContainer.textContent = "";
+    HTMLPinnedTabContainer.innerHTML = "";
     HTMLRegularTabContainer.innerHTML = "";
 
     let TabObjects = await browser.tabs.query({ currentWindow: true });
@@ -16,86 +16,129 @@ async function TabList()
         {
             let HTMLTab = document.createElement("div");
                 HTMLTab.classList.add("tabbrowser-tab");
-                HTMLTab.setAttribute("pinned", TabObject.pinned);
-                HTMLTab.setAttribute("visuallyselected", TabObject.active);
                 HTMLTab.setAttribute("id", TabObject.id);
+                HTMLTab.setAttribute("visuallyselected", TabObject.active);
+                HTMLTab.setAttribute("pinned", TabObject.pinned);
                 HTMLTab.setAttribute("soundplaying", TabObject.audible);
                 HTMLTab.setAttribute("muted", TabObject.mutedInfo.muted);
                 HTMLPinnedTabContainer.appendChild(HTMLTab);
 
-                let HTMLTabBackground = document.createElement("div");
-                    HTMLTabBackground.classList.add("tab-background");
-                    HTMLTab.appendChild(HTMLTabBackground);
+                let HTMLTabStack = document.createElement("div");
+                    HTMLTabStack.classList.add("tab-stack");
+                    HTMLTab.appendChild(HTMLTabStack);
+
+                    let HTMLTabBackground = document.createElement("div");
+                        HTMLTabBackground.classList.add("tab-background");
+                        HTMLTabStack.appendChild(HTMLTabBackground);
+
+                        let HTMLTabContextLine = document.createElement("div");
+                            HTMLTabContextLine.classList.add("tab-context-line");
+                            HTMLTabBackground.appendChild(HTMLTabContextLine);
+
+                        let HTMLTabLoadingBurst = document.createElement("div");
+                            HTMLTabLoadingBurst.classList.add("tab-loading-burst");
+                            HTMLTabBackground.appendChild(HTMLTabLoadingBurst);
 
                     let HTMLTabContent = document.createElement("div");
                         HTMLTabContent.classList.add("tab-content");
-                        HTMLTabBackground.appendChild(HTMLTabContent);
+                        HTMLTabStack.appendChild(HTMLTabContent);
 
                         let HTMLTabIconStack = document.createElement("div");
                             HTMLTabIconStack.classList.add("tab-icon-stack");
                             HTMLTabContent.appendChild(HTMLTabIconStack);
 
-                            let HTMLTabIcon = document.createElement("img");
-                                HTMLTabIcon.classList.add("tab-icon");
-                                HTMLTabIcon.src = TabObject.favIconUrl;
-                                HTMLTabIconStack.appendChild(HTMLTabIcon);
+                            let HTMLTabThrobber = document.createElement("img");
+                                HTMLTabThrobber.classList.add("tab-throbber");
+                                HTMLTabIconStack.appendChild(HTMLTabThrobber);
 
-                            let HTMLTabIconSound = document.createElement("img");
-                                HTMLTabIconSound.classList.add("tab-icon-sound");
-                                HTMLTabIconStack.appendChild(HTMLTabIconSound);
+                            let HTMLTabIconPending = document.createElement("img");
+                                HTMLTabIconPending.classList.add("tab-icon-pending");
+                                HTMLTabIconStack.appendChild(HTMLTabIconPending);
+
+                            let HTMLTabIconImage = document.createElement("img");
+                                HTMLTabIconImage.classList.add("tab-icon-image");
+                                HTMLTabIconImage.src = TabObject.favIconUrl;
+                                HTMLTabIconStack.appendChild(HTMLTabIconImage);
+
+                            let HTMLTabSharingIconOverlay = document.createElement("img");
+                                HTMLTabSharingIconOverlay.classList.add("tab-sharing-icon-overlay");
+                                HTMLTabIconStack.appendChild(HTMLTabSharingIconOverlay);
+
+                            let HTMLTabIconOverlay = document.createElement("img");
+                                HTMLTabIconOverlay.classList.add("tab-icon-overlay");
+                                HTMLTabIconOverlay.setAttribute("id", TabObject.id);
+                                HTMLTabIconStack.appendChild(HTMLTabIconOverlay);
         }
 
-        if (!TabObject.pinned)
+        else if (!TabObject.pinned)
         {
             let HTMLTab = document.createElement("div");
                 HTMLTab.classList.add("tabbrowser-tab");
-                HTMLTab.setAttribute("pinned", TabObject.pinned);
-                HTMLTab.setAttribute("visuallyselected", TabObject.active);
                 HTMLTab.setAttribute("id", TabObject.id);
+                HTMLTab.setAttribute("visuallyselected", TabObject.active);
+                HTMLTab.setAttribute("pinned", TabObject.pinned);
                 HTMLTab.setAttribute("soundplaying", TabObject.audible);
                 HTMLTab.setAttribute("muted", TabObject.mutedInfo.muted);
                 HTMLRegularTabContainer.appendChild(HTMLTab);
 
-                let HTMLTabBackground = document.createElement("div");
-                    HTMLTabBackground.classList.add("tab-background");
-                    HTMLTab.appendChild(HTMLTabBackground);
+                let HTMLTabStack = document.createElement("div");
+                    HTMLTabStack.classList.add("tab-stack");
+                    HTMLTab.appendChild(HTMLTabStack);
+
+                    let HTMLTabBackground = document.createElement("div");
+                        HTMLTabBackground.classList.add("tab-background");
+                        HTMLTabStack.appendChild(HTMLTabBackground);
+
+                        let HTMLTabContextLine = document.createElement("div");
+                            HTMLTabContextLine.classList.add("tab-context-line");
+                            HTMLTabBackground.appendChild(HTMLTabContextLine);
+
+                        let HTMLTabLoadingBurst = document.createElement("div");
+                            HTMLTabLoadingBurst.classList.add("tab-loading-burst");
+                            HTMLTabBackground.appendChild(HTMLTabLoadingBurst);
 
                     let HTMLTabContent = document.createElement("div");
                         HTMLTabContent.classList.add("tab-content");
-                        HTMLTabBackground.appendChild(HTMLTabContent);
-
-
-
+                        HTMLTabStack.appendChild(HTMLTabContent);
 
                         let HTMLTabIconStack = document.createElement("div");
                             HTMLTabIconStack.classList.add("tab-icon-stack");
                             HTMLTabContent.appendChild(HTMLTabIconStack);
 
-                            let HTMLTabIcon = document.createElement("img");
-                                HTMLTabIcon.classList.add("tab-icon");
-                                HTMLTabIcon.src = TabObject.favIconUrl;
-                                HTMLTabIconStack.appendChild(HTMLTabIcon);
+                            let HTMLTabThrobber = document.createElement("img");
+                                HTMLTabThrobber.classList.add("tab-throbber");
+                                HTMLTabIconStack.appendChild(HTMLTabThrobber);
 
-                            let HTMLTabIconSound = document.createElement("img");
-                                HTMLTabIconSound.classList.add("tab-icon-sound");
-                                HTMLTabIconStack.appendChild(HTMLTabIconSound);
+                            let HTMLTabIconPending = document.createElement("img");
+                                HTMLTabIconPending.classList.add("tab-icon-pending");
+                                HTMLTabIconStack.appendChild(HTMLTabIconPending);
 
+                            let HTMLTabIconImage = document.createElement("img");
+                                HTMLTabIconImage.classList.add("tab-icon-image");
+                                HTMLTabIconImage.src = TabObject.favIconUrl;
+                                HTMLTabIconStack.appendChild(HTMLTabIconImage);
 
+                            let HTMLTabSharingIconOverlay = document.createElement("img");
+                                HTMLTabSharingIconOverlay.classList.add("tab-sharing-icon-overlay");
+                                HTMLTabIconStack.appendChild(HTMLTabSharingIconOverlay);
 
+                            let HTMLTabIconOverlay = document.createElement("img");
+                                HTMLTabIconOverlay.classList.add("tab-icon-overlay");
+                                HTMLTabIconOverlay.setAttribute("id", TabObject.id);
+                                HTMLTabIconStack.appendChild(HTMLTabIconOverlay);
 
+                        let HTMLTabLabelContainer = document.createElement("div");
+                            HTMLTabLabelContainer.classList.add("tab-label-container");
+                            HTMLTabContent.appendChild(HTMLTabLabelContainer);
 
-
-
-
-
-                        let HTMLTabText = document.createElement("div");
-                            HTMLTabText.classList.add("tab-text");
-                            HTMLTabText.innerHTML = TabObject.title;
-                            HTMLTabContent.appendChild(HTMLTabText);
+                            let HTMLTabLabel = document.createElement("div");
+                                HTMLTabLabel.classList.add("tab-label");
+                                HTMLTabLabel.innerHTML = TabObject.title;
+                                HTMLTabLabelContainer.appendChild(HTMLTabLabel);
 
                         let HTMLTabCloseButton = document.createElement("div");
                             HTMLTabCloseButton.classList.add("tab-close-button");
-HTMLTabCloseButton.setAttribute("id", TabObject.id);
+                            HTMLTabCloseButton.setAttribute("id", TabObject.id);
                             HTMLTabContent.appendChild(HTMLTabCloseButton);
         }
     }
@@ -108,19 +151,62 @@ HTMLTabCloseButton.setAttribute("id", TabObject.id);
 
 
 
+
+
+
+
+
+
+
 document.addEventListener("click", (e) =>
 {
-
-
-
     /* ---------- Selected Tab (Internal Event) ---------- */
 
-    if (e.target.hasAttribute("visuallyselected"))
+    if (e.target.classList.contains("tabbrowser-tab"))
     {
         let TabObjectId = parseInt(e.target.getAttribute("id"));
 
         browser.tabs.update(TabObjectId, { active: true });
     }
+
+
+
+
+
+
+
+
+
+
+    /* ---------- Disable Tab Sound (Internal Event) ---------- */
+
+    if (e.target.classList.contains("tab-icon-overlay"))
+    {
+        let TabObjectId = parseInt(e.target.getAttribute("id"));
+
+        e.target.setAttribute("muted", true);
+
+        browser.tabs.update(TabObjectId, { muted: true });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /* ---------- Close Tab (Internal Event) ---------- */
 
@@ -130,15 +216,10 @@ document.addEventListener("click", (e) =>
 
         browser.tabs.remove(TabObjectId);
     }
-
-
-
-
-
-
-
-
 });
+
+
+
 
 
 
@@ -150,15 +231,15 @@ document.addEventListener("click", (e) =>
 
 /* ---------- Selected Tab (External Event) ---------- */
 
-browser.tabs.onActivated.addListener(TabList);
+//browser.tabs.onActivated.addListener(TabList);
 
 /* ---------- Close Tab (External Event) ---------- */
 
-browser.tabs.onRemoved.addListener(TabList);
+//browser.tabs.onRemoved.addListener(TabList);
 
 /* ---------- Update Tab ---------- */
 
-browser.tabs.onUpdated.addListener(TabList);
+//browser.tabs.onUpdated.addListener(TabList);
 
 
 
